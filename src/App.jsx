fadca90aa253e9d1ee41282efd9a9c3336133878
Leandro De Torres <leandrodetorres@ip-10-28-2-100.ap-southeast-1.compute.internal>
2026-01-21 18:50:03 +0700

    feat: add hello world

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <p className="read-the-docs">
+            Hello world!
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default App
